ebdac56a8a6fa7cea0cb71c19dad233bec3f5475
Alexander Lebedev <alexander.s.lebedev@gmail.com>
2019-10-25 22:01:34 +0700

    Initial Commit

---
+++ ShoppingList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShoppingList
+//
+//  Created by alexander on 25.10.2019.
+//  Copyright © 2019 alexander. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
